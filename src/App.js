05d717cd8e95c19a255c7bf73161a8d384df3046
Chad Dyck <chad@chaddyck.com>
2020-07-30 16:45:14 -0600

    Added search input and 'find me' button to UI.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Container from './components/container';
 import Overlay from './components/overlay';
 
 function App() {
-	const getAPI = (lat, lng) => {
+	const getVenues = (lat, lng) => {
 		const endPoint = "https://api.foursquare.com/v2/venues/explore?";
 		const params = {
 			client_id: "NAVENW2HU2XT13XMVHCII1JKPRLTXCWEV0TXT1NF2Z4BCZNI",
@@ -33,9 +33,13 @@ function App() {
 	const [loc, setLoc] = useState(false);
 	return (
 		<div className="App">
-			<button className={`findMeBtn ${loc ? 'hideBtn' : ''}`} onClick={() => getAPI(lat, lng)} ></button>
+			<div className="searchContainer">
+				<button className="findBtn" ><span role="img" aria-label="location">&#x1F4CD;</span> Find me</button>
+				<input className="searchBar" placeholder="Enter your location"></input>
+				<button className={`witfBtn ${!loc || (lat === 0 && lng === 0) ? 'hideBtn' : ''}`} onClick={() => getVenues(lat, lng)} ></button>
+			</div>
 			<Container list={list} />
-			<Overlay
+			<Overlay style={{display: !loc ? 'none' : 'block'}}
 				list={list}
 				lat={lat}
 				lng={lng}
