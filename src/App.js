cc051a1b305234bcf24433f74d2e91def3cc545d
Chad Dyck <chad@chaddyck.com>
2020-07-30 14:37:34 -0600

    Changed classes to functional components w/ hooks.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Axios from 'axios';
 import Container from './components/container';
 import Overlay from './components/overlay';
 
-export default class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			list: [],
-			lat: 0,
-			lng: 0,
-			locationFound: false
-		}
-	}
-	getAPI = (lat, lng) => {
+function App() {
+	const getAPI = (lat, lng) => {
 		const endPoint = "https://api.foursquare.com/v2/venues/explore?";
 		const params = {
 			client_id: "NAVENW2HU2XT13XMVHCII1JKPRLTXCWEV0TXT1NF2Z4BCZNI",
@@ -26,32 +17,32 @@ export default class App extends React.Component {
 			v: 20180323
 		}
 		Axios.get(endPoint + new URLSearchParams(params)).then(response => {
-			this.setState({
-				list: response.data.response.groups[0].items,
-				locationFound: true 
-			})
+			setList(response.data.response.groups[0].items);
+			setLoc(true);
 		})
 	}
-	componentDidMount() {
+	useEffect(() => {
 		navigator.geolocation.getCurrentPosition((position) => {
-			this.setState({
-				lat: position.coords.latitude,
-				lng: position.coords.longitude
-			})
+			setLat(position.coords.latitude);
+			setLng(position.coords.longitude);
 		})
-	}
-	render() {
-		return (
-			<div className="App">
-				<button className={`findMeBtn ${this.state.locationFound ? 'hideBtn' : ''}`} onClick={() => this.getAPI(this.state.lat, this.state.lng)} ></button>
-				<Container list={this.state.list} />
-				<Overlay
-					list={this.state.list}
-					lat={this.state.lat}
-					lng={this.state.lng}
-					locationFound={this.state.locationFound}
-				/>
-			</div>
-		)
-	}
+	})
+	const [list, setList] = useState([]);
+	const [lat, setLat] = useState(0);
+	const [lng, setLng] = useState(0);
+	const [loc, setLoc] = useState(false);
+	return (
+		<div className="App">
+			<button className={`findMeBtn ${loc ? 'hideBtn' : ''}`} onClick={() => getAPI(lat, lng)} ></button>
+			<Container list={list} />
+			<Overlay
+				list={list}
+				lat={lat}
+				lng={lng}
+				loc={loc}
+			/>
+		</div>
+	)
 }
+
+export default App;
--- src/components/container.js
+++ src/components/container.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Venue from './venue';
 
-export default class Container extends React.Component {
-	render() {
-		return (
-			<div className="container">
-				{this.props.list.map((venue, index) => {
-					return <Venue
-						info={venue}
-						key={index}
-					/>
-				})}
-			</div>
-		);
-	}
+function Container({list}) {
+	return (
+		<div className="container">
+			{list.map((venue, index) => {
+				return <Venue
+					info={venue}
+					key={index}
+				/>
+			})}
+		</div>
+	);
 }
+
+export default Container;
