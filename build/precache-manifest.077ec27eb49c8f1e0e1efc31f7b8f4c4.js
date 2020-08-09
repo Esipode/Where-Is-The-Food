f3d851b4763a3012e79fba3cd5058dd4809abe50
Chad Dyck <chad@chaddyck.com>
2020-08-09 09:41:07 -0600

    Release V.1.0

---
+++ build/precache-manifest.077ec27eb49c8f1e0e1efc31f7b8f4c4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721cce0c700cf1c4972908dfd34308ec",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a69ea457d226be16f059",
+    "url": "./static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "ec6bfc42b1a66ff44d92",
+    "url": "./static/css/main.ae0abf1d.chunk.css"
+  },
+  {
+    "revision": "a69ea457d226be16f059",
+    "url": "./static/js/2.12d0f38b.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "./static/js/2.12d0f38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec6bfc42b1a66ff44d92",
+    "url": "./static/js/main.67ca38a7.chunk.js"
+  },
+  {
+    "revision": "ce7ccc270043c3c2ed33",
+    "url": "./static/js/runtime-main.9d8d1f65.js"
+  },
+  {
+    "revision": "78dbfcd031044f8e3c2f910d586faa01",
+    "url": "./static/media/logo.78dbfcd0.svg"
+  }
+]);
